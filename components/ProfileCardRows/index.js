2685882b108fbfca97876e0f4b4ff38c6033807c
Pranjal Bareth <pranjalnirmala@gmail.com>
2022-06-04 22:03:36 +0000

    Profile-Edit-Feature

--- components/ProfileCardRows/index.js
+++ components/ProfileCardRows/index.js
@@ -6,8 +6,26 @@ import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import MailIcon from "@mui/icons-material/Mail";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal';
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 750,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const ProfileCardRows = (props) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const {
     leftLogo,
     leftHeading,
@@ -51,10 +69,38 @@ const ProfileCardRows = (props) => {
           </div>
           <div className={styles.editContainer}>
             <img
+            onClick={handleOpen}
               className={styles.rightLogo}
               src={"https://i.imgur.com/JR7Wtov.png"}
               alt="Edit"
             />
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box 
+              className={styles.EditModal} sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Upload Resume
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2, color:"rgba(0,0,0,.65)" }}>
+                Upload your resume below and we'll try to automatically parse your information for the next steps. Don't worry if your resume isn't perfect, we'll help you polish it later.
+                </Typography>
+                <Button
+                  sx={{ marginTop: '26px', boxShadow: "none"}}
+                  variant="contained"
+                  component="label"
+                >
+                  Upload File
+                  <input
+                    type="file"
+                    hidden
+                  />
+                </Button>
+              </Box>
+            </Modal>
           </div>
         </div>
       </div>
