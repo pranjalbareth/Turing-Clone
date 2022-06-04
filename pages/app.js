abb9182002ef683c785e461c21a05741185a6c08
Pranjal Bareth <pranjalnirmala@gmail.com>
2022-06-04 22:05:07 +0000

    Updated-Settings

--- pages/app.js
+++ pages/app.js
@@ -1,5 +1,13 @@
 import IndexPage from "./index";
+import Head from "next/head";
 
 export default function App() {
-  return <IndexPage />;
+  return( <>
+      <Head>
+        <title>Turing Remote Developer Jobs | Apply to Elite U.S. Companies</title>
+        <link rel="icon" href="https://i.imgur.com/YZA9s2o.png" />
+      </Head>
+      <IndexPage />
+  </>
+)
 }
